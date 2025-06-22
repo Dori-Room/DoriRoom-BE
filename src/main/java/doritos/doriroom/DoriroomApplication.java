4b44a8fea358db1e37d951e606deed999e91524a
docherryra <docherryra@gmail.com>
2025-06-22 20:46:54 +0900

    initial settings

---
+++ src/main/java/doritos/doriroom/DoriroomApplication.java
@@ -0,0 +1,13 @@
+package doritos.doriroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoriroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoriroomApplication.class, args);
+	}
+
+}
