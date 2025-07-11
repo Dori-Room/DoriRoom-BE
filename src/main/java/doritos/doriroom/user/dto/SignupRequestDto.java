571c94f90bec2568db9771e166053ce3c489336e
docherryra <docherryra@gmail.com>
2025-07-11 15:11:02 +0900

    feat: 회원가입 DTO 설계

---
+++ src/main/java/doritos/doriroom/user/dto/SignupRequestDto.java
@@ -0,0 +1,8 @@
+package doritos.doriroom.user.dto;
+
+public class SignupRequestDto {
+    private String username;
+    private String password;
+    private String nickname;
+    // private String profileImageUrl;
+}
