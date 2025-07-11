b54b613c3b7dcacc9d65279011316398bed9196b
docherryra <docherryra@gmail.com>
2025-07-11 20:45:23 +0900

    feat: SecurityConfig PasswordEndcoder Bean 등록

--- src/main/java/doritos/doriroom/global/security/SecurityConfig.java
+++ src/main/java/doritos/doriroom/global/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,4 +31,8 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
