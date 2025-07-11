e65d8fb3db3f688690a53837e14cbbf768337961
docherryra <docherryra@gmail.com>
2025-07-11 21:03:18 +0900

    feat: 회원가입 API controller 구현 및 validation 검사 추가

--- src/main/java/doritos/doriroom/user/dto/SignupRequestDto.java
+++ src/main/java/doritos/doriroom/user/dto/SignupRequestDto.java
@@ -1,10 +1,15 @@
 package doritos.doriroom.user.dto;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 public class SignupRequestDto {
+
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    @NotBlank
     private String nickname;
     // private String profileImageUrl;
 }
