ff4161549670678e6c1be23de635ba0172783f8b
docherryra <docherryra@gmail.com>
2025-07-11 18:19:28 +0900

    feat: user repository 구현

--- src/main/java/doritos/doriroom/user/repository/UserRepository.java
+++ src/main/java/doritos/doriroom/user/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package doritos.doriroom.user.repository;
 
-public class UserRepository {
+import doritos.doriroom.user.domain.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    // username, nickname 중복 검사
+    boolean existsByUsername(String username);
+    boolean existsByNickname(String nickname);
+
+    // 로그인 시 유저 조회
+    User findByUsername(String username);
 }
