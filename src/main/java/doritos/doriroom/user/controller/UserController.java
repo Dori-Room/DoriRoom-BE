19878ed9b31dccd3d4d2b3463846e85a3ab851eb
IyeonHan <104899885+docherryra@users.noreply.github.com>
2025-07-12 21:08:13 +0900

    Revert "feat: 회원가입 API controller 구현 및 validation 검사 추가"
    This reverts commit f15e7e789776f8c7da4d476e05fcb12c5a3cc530.

--- src/main/java/doritos/doriroom/user/controller/UserController.java
+++ src/main/java/doritos/doriroom/user/controller/UserController.java
@@ -1,27 +1,4 @@
 package doritos.doriroom.user.controller;
 
-
-import doritos.doriroom.global.dto.ApiResponse;
-import doritos.doriroom.user.dto.SignupRequestDto;
-import doritos.doriroom.user.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/users")
-@RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
-
-    @PostMapping("/signup")
-    @Operation(summary = "회원가입")
-    public ApiResponse<Void> signup(@RequestBody @Valid SignupRequestDto request){
-        userService.signup(request);
-        return ApiResponse.ok();
-    }
 }
--- src/main/java/doritos/doriroom/user/dto/SignupRequestDto.java
+++ src/main/java/doritos/doriroom/user/dto/SignupRequestDto.java
@@ -1,15 +1,10 @@
 package doritos.doriroom.user.dto;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 public class SignupRequestDto {
-
-    @NotBlank
     private String username;
-    @NotBlank
     private String password;
-    @NotBlank
     private String nickname;
     // private String profileImageUrl;
 }
