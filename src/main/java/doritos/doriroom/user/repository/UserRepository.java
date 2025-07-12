04fe4e937f723b1955bf116e0d111227d8b62353
IyeonHan <104899885+docherryra@users.noreply.github.com>
2025-07-12 21:08:13 +0900

    Revert "feat: user repository 구현"
    This reverts commit 83bd5bca52be65174be1c26627439c283b26b34c.

--- src/main/java/doritos/doriroom/user/repository/UserRepository.java
+++ src/main/java/doritos/doriroom/user/repository/UserRepository.java
@@ -1,16 +1,4 @@
 package doritos.doriroom.user.repository;
 
-import doritos.doriroom.user.domain.User;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-    // username, nickname 중복 검사
-    boolean existsByUsername(String username);
-    boolean existsByNickname(String nickname);
-
-    // 로그인 시 유저 조회
-    User findByUsername(String username);
+public class UserRepository {
 }
