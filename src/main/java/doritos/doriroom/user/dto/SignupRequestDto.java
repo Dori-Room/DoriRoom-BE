40f478d409c6e28dad45392e9eeb03527d44217c
IyeonHan <104899885+docherryra@users.noreply.github.com>
2025-07-12 21:08:13 +0900

    Revert "feat: 회원가입 DTO 설계"
    This reverts commit 4ea88705ec45eaeae091f8c18f8472c2b40b5d13.

--- src/main/java/doritos/doriroom/user/dto/SignupRequestDto.java
+++
@@ -1,8 +0,0 @@
-package doritos.doriroom.user.dto;
-
-public class SignupRequestDto {
-    private String username;
-    private String password;
-    private String nickname;
-    // private String profileImageUrl;
-}
