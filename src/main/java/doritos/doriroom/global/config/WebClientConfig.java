87bdcf32803d1370d887cf47714dd3b182d0d389
judyzero <qozmcll@kookmin.ac.kr>
2025-07-14 11:17:39 +0900

    feat: Tour API 호출을 위한 WebClient 설정 클래스 추가

---
+++ src/main/java/doritos/doriroom/global/config/WebClientConfig.java
@@ -0,0 +1,24 @@
+package doritos.doriroom.global.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory.EncodingMode;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient() {
+        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory("https://apis.data.go.kr");
+        uriBuilderFactory.setEncodingMode(EncodingMode.NONE);
+
+        return WebClient.builder()
+            .uriBuilderFactory(uriBuilderFactory)
+            .baseUrl("https://apis.data.go.kr")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+}
