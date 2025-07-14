ce15f1889f3627e89c4bfbf09aa9e010b8cdbd11
judyzero <qozmcll@kookmin.ac.kr>
2025-07-14 22:17:02 +0900

    feat: contentId 기준으로 축제 내용 검색

---
+++ src/main/java/doritos/doriroom/event/repository/EventRepository.java
@@ -0,0 +1,13 @@
+package doritos.doriroom.event.repository;
+
+import doritos.doriroom.event.domain.Event;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+    @Query("SELECT e.contentId FROM Event e WHERE e.contentId IN :contentIds")
+    List<String> findAllContentIdIn(@Param("contentIds") List<String> contentIds);
+}
