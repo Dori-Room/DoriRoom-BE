97da67b99926eddd6567829d5004a22bf0be297f
judyzero <qozmcll@kookmin.ac.kr>
2025-07-19 22:06:42 +0900

    feat: 신규 축제 조회 API 추가

--- src/main/java/doritos/doriroom/event/repository/EventRepository.java
+++ src/main/java/doritos/doriroom/event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package doritos.doriroom.event.repository;
 import doritos.doriroom.event.domain.Event;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,12 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
 
     @Query("SELECT e FROM Event e WHERE e.contentId IN :contentIds")
     List<Event> findEventsByContentIds(@Param("contentIds") List<String> contentIds);
+
+    @Query("""
+    SELECT e FROM Event e
+    WHERE e.startDate = CURRENT_DATE
+    ORDER BY e.startDate ASC
+    """)
+    List<Event> findUpcomingEvents(Pageable pageable);
+
 }
