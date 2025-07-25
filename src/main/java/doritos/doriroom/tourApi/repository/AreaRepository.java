c0713dcfac3f516123a9359aca32e51cd49d864f
judyzero <qozmcll@kookmin.ac.kr>
2025-07-26 00:16:40 +0900

    feat: 지역 정보 저장 기능 구현

---
+++ src/main/java/doritos/doriroom/tourApi/repository/AreaRepository.java
@@ -0,0 +1,7 @@
+package doritos.doriroom.tourApi.repository;
+
+import doritos.doriroom.tourApi.domain.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<Area, Integer> {
+} 
