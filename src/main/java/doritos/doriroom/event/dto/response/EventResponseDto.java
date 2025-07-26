5f05fe8b3cb77e9ff2fae51ca975afb5e335b8d8
judyzero <qozmcll@kookmin.ac.kr>
2025-07-26 15:21:27 +0900

    feat: 축제 미리보기에 지역 필드 추가

--- src/main/java/doritos/doriroom/event/dto/response/EventResponseDto.java
+++ src/main/java/doritos/doriroom/event/dto/response/EventResponseDto.java
@@ -1,14 +1,19 @@
 package doritos.doriroom.event.dto.response;
 
 import doritos.doriroom.event.domain.Event;
+import doritos.doriroom.tourApi.domain.AreaGroup;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public record EventResponseDto(
     String title,
     int contentId,
+    UUID eventId,
     String startDate,
     String endDate,
     String addr1,
+    String areaName,
+    int areaCode,
     String firstImage,
     String secondImage
 ) {
@@ -16,9 +21,12 @@ public record EventResponseDto(
         return new EventResponseDto(
             e.getTitle(),
             e.getContentId(),
+            e.getEventId(),
             e.getStartDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
             e.getEndDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
             e.getAddr1(),
+            AreaGroup.getAreaNameByAreaCode(e.getAreaCode()),
+            e.getAreaCode(),
             e.getFirstImage(),
             e.getSecondImage()
         );
