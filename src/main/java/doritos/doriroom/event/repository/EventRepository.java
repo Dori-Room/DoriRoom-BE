32df8dcc51674be918c3f9acd1f92ac3c4ab3954
judyzero <qozmcll@kookmin.ac.kr>
2025-07-30 00:31:33 +0900

    feat: 도별 축제 조회 기능 구현

--- src/main/java/doritos/doriroom/event/repository/EventRepository.java
+++ src/main/java/doritos/doriroom/event/repository/EventRepository.java
@@ -4,6 +4,7 @@ import doritos.doriroom.event.domain.Event;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -27,4 +28,12 @@ public interface EventRepository extends JpaRepository<Event, UUID>, EventReposi
         "WHERE e.startDate <= :today AND e.endDate >= :today " +
         "ORDER BY e.endDate ASC")
     List<Event> findEndingSoonEvents(@Param("today") LocalDate today, Pageable pageable);
+
+    @Query("""
+        SELECT e FROM Event e
+        WHERE e.areaCode IN :areaCodes
+        AND e.endDate >= CURRENT_DATE
+        ORDER BY e.startDate ASC
+    """)
+    Page<Event> findByAreaCodesIn(@Param("areaCodes") List<Integer> areaCodes, Pageable pageable);
 }
