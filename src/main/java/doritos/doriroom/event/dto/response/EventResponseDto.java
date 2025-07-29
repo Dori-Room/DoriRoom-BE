a896f3e40469862d6e7e3b559cfff80eef819cab
judyzero <qozmcll@kookmin.ac.kr>
2025-07-30 00:55:20 +0900

    feat: 축제 응답 DTO에 카테고리 추가

--- src/main/java/doritos/doriroom/event/dto/response/EventResponseDto.java
+++ src/main/java/doritos/doriroom/event/dto/response/EventResponseDto.java
@@ -2,6 +2,7 @@ package doritos.doriroom.event.dto.response;
 
 import doritos.doriroom.event.domain.Event;
 import doritos.doriroom.tourApi.domain.AreaGroup;
+import doritos.doriroom.tourApi.domain.Category;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
@@ -14,10 +15,13 @@ public record EventResponseDto(
     String addr1,
     String areaName,
     int areaCode,
+    String categoryName,
     String firstImage,
     String secondImage
 ) {
     public static EventResponseDto from(Event e) {
+        String categoryCode = getCategoryCode(e.getLclsSystm2(), e.getLclsSystm3());
+
         return new EventResponseDto(
             e.getTitle(),
             e.getContentId(),
@@ -27,8 +31,27 @@ public record EventResponseDto(
             e.getAddr1(),
             AreaGroup.getAreaNameByAreaCode(e.getAreaCode()),
             e.getAreaCode(),
+            getCategoryName(categoryCode),
             e.getFirstImage(),
             e.getSecondImage()
         );
     }
+
+    private static String getCategoryCode(String lclsSystm2, String lclsSystm3) {
+        if(lclsSystm2.startsWith("EV01")) {
+            return lclsSystm3;
+        }
+        return lclsSystm2;
+    }
+
+    private static String getCategoryName(String categoryCode) {
+        for (Category category : Category.values()) {
+            if (category.getCode().equals(categoryCode)) {
+                return category.getName();
+            }
+        }
+        return "기타";
+    }
+
+
 }
