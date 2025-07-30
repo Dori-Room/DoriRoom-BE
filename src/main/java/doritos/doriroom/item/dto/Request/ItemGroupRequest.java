b3bcf7c152d3887ad1a849110372b36c5c5b6c37
docherryra <docherryra@gmail.com>
2025-07-31 04:56:48 +0900

    refactor: 사용하지 않는 dto 삭제, swagger 설명 첨부

--- src/main/java/doritos/doriroom/item/dto/Request/ItemGroupRequest.java
+++
@@ -1,9 +0,0 @@
-package doritos.doriroom.item.dto.Request;
-
-import doritos.doriroom.item.domain.ItemGroup;
-import jakarta.validation.constraints.NotBlank;
-
-public record ItemGroupRequest (
-        @NotBlank
-        ItemGroup group
-){ }
