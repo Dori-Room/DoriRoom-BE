ec7cd3cead4a5c0b0c2a1720e8e67448fdc90957
docherryra <docherryra@gmail.com>
2025-08-02 00:19:13 +0900

    feat: @Valid 유효성 검사 추가

--- src/main/java/doritos/doriroom/user/dto/RefreshTokenRequestDto.java
+++ src/main/java/doritos/doriroom/user/dto/RefreshTokenRequestDto.java
@@ -1,8 +1,10 @@
 package doritos.doriroom.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class RefreshTokenRequestDto {
+    @NotBlank
     private String refreshToken;
 }
