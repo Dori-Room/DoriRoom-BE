59b9eef52a377996b5fcbcb14e7297c077ac295a
judyzero <qozmcll@kookmin.ac.kr>
2025-08-02 18:46:15 +0900

    comment: 주석 삭제

--- src/main/java/doritos/doriroom/tourApi/dto/response/TourApiDetailInfoDto.java
+++ src/main/java/doritos/doriroom/tourApi/dto/response/TourApiDetailInfoDto.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class TourApiDetailInfoDto {
     private String contentid;
-    private String infoname;    // "행사소개" or "행사내용"
+    private String infoname;
     private String infotext;
 }
