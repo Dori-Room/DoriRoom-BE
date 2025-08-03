ddd1f49c9a077f2649e6066b96d16a7eea580ca9
docherryra <docherryra@gmail.com>
2025-08-04 00:30:54 +0900

    fix: group 예약어 필드명 변경 및 통일

--- src/main/java/doritos/doriroom/item/dto/response/EquipItemResponse.java
+++ src/main/java/doritos/doriroom/item/dto/response/EquipItemResponse.java
@@ -7,15 +7,15 @@ import lombok.Builder;
 public record EquipItemResponse(
         Long equippedItemId,
         String name,
-        ItemType type,
+        ItemType itemType,
         boolean isEquipped
 
 ) {
-    private static EquipItemResponse from(Long equippedItemId, String name, ItemType type, boolean equipped) {
+    private static EquipItemResponse from(Long equippedItemId, String name, ItemType itemType, boolean equipped) {
         return EquipItemResponse.builder()
                 .equippedItemId(equippedItemId)
                 .name(name)
-                .type(type)
+                .itemType(itemType)
                 .isEquipped(equipped)
                 .build();
     }
--- src/main/java/doritos/doriroom/item/dto/response/PaymentViewResponse.java
+++ src/main/java/doritos/doriroom/item/dto/response/PaymentViewResponse.java
@@ -5,19 +5,10 @@ import lombok.Builder;
 
 @Builder
 public record PaymentViewResponse(
-//        Long itemId,
-//        String imageUrl,
-//        ItemGroup group,
-//        CollectionTheme theme,
-//        ItemType type,
-//        boolean isPurchasable,
-//        boolean isOwned,
-
         String name,
         Long price,
         Long remainingCredit,
         boolean isBuyable // 보유 크레딧 상태에 따른 구매 가능 여부
-
 )
 {
     public static PaymentViewResponse from(Item item, Long remainingCredit){
