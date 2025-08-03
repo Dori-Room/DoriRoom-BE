4ef9c4e864f735960a91c9b20c97320b8272c3a5
docherryra <docherryra@gmail.com>
2025-08-04 01:24:32 +0900

    feat: 인증 필터를 Bean으로 등록

--- src/main/java/doritos/doriroom/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/doritos/doriroom/global/jwt/JwtAuthenticationFilter.java
@@ -9,11 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.rmi.server.ServerCloneException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
