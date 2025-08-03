1fdac52b372e84f1de3b23fd39efaeabcdf2f567
docherryra <docherryra@gmail.com>
2025-08-04 03:53:22 +0900

    feat: 토큰 검증 실패 예외 처리 추가

--- src/main/java/doritos/doriroom/global/jwt/TokenExpiredException.java
+++ src/main/java/doritos/doriroom/global/jwt/TokenExpiredException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class TokenExpiredException extends ApiException {
     public TokenExpiredException() {
-        super(HttpStatus.BAD_REQUEST, "만료된 토큰 입니다.");
+        super(HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다.");
     }
 }
