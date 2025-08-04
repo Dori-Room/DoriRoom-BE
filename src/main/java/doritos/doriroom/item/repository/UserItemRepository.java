77ad39fe0029eb217bc17856d63b38168f7cd65f
docherryra <docherryra@gmail.com>
2025-08-05 04:02:26 +0900

    feat(item): type별 아이템 조회 api 구현

--- src/main/java/doritos/doriroom/item/repository/UserItemRepository.java
+++ src/main/java/doritos/doriroom/item/repository/UserItemRepository.java
@@ -15,6 +15,7 @@ public interface UserItemRepository extends JpaRepository<UserItem, Long> {
     List<UserItem> findByUser(User user); // 유저가 보유한 전체 아이템 조회
     List<UserItem> findByUserAndItem_ItemGroup(User user, ItemGroup itemGroup); // 유저가 보유한 COMMON or AREA 아이템 조회
     List<UserItem> findByUserAndItem_ItemGroupAndItem_AreaGroup(User user, ItemGroup itemGroup, AreaGroup areaGroup); // 유저의 특정 지역별 아이템 조회
+    List<UserItem> findByUserAndItem_ItemType(User user, ItemType itemType); // 유저가 보유한 타입별 아이템 조회
 
     Optional<UserItem> findByUserAndItem(User user, Item item); // 유저가 보유한 아이템 단일 조회
     Optional<UserItem> findByUserAndItem_ItemTypeAndIsEquippedTrue(User user, ItemType itemType); // 유저가 착용한 타입별 아이템 조회
