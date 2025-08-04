6e624e06a751bae0fde9ba8eec8cbf2dd3aed74d
judyzero <qozmcll@kookmin.ac.kr>
2025-08-04 22:49:51 +0900

    feat: swagger UI에 Authorize 토큰 넣을 수 있도록 기능 추가

--- src/main/java/doritos/doriroom/global/swagger/SwaggerConfig.java
+++ src/main/java/doritos/doriroom/global/swagger/SwaggerConfig.java
@@ -3,6 +3,8 @@ package doritos.doriroom.global.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,7 +15,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .components(new Components())
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
             .info(apiInfo());
     }
 
