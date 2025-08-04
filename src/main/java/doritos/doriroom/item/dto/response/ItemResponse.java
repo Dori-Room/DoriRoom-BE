f7568d7a34f76997c771c7fb7811f0793ec09220
docherryra <docherryra@gmail.com>
2025-08-05 02:14:27 +0900

    feat(dto): areaGroup을 포함하도록 dto 수정, null을 JSON응답에서 보이지 않도록 처리(undefined)

--- src/main/java/doritos/doriroom/item/dto/response/ItemResponse.java
+++ src/main/java/doritos/doriroom/item/dto/response/ItemResponse.java
@@ -1,18 +1,22 @@
 package doritos.doriroom.item.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import doritos.doriroom.item.domain.CollectionTheme;
 import doritos.doriroom.item.domain.Item;
 import doritos.doriroom.item.domain.ItemGroup;
 import doritos.doriroom.item.domain.ItemType;
+import doritos.doriroom.tourApi.domain.AreaGroup;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ItemResponse (
         Long itemId,
         String name,
         String imageUrl,
         ItemType itemType,
-        ItemGroup itemGroup,
+        ItemGroup itemGroup, // COMMON or AREA
+        AreaGroup areaGroup, // nullable
         CollectionTheme theme, // nullable
         Long price,
         boolean isPurchasable,
@@ -25,6 +29,7 @@ public record ItemResponse (
                 .imageUrl(i.getImageUrl())
                 .itemType(i.getItemType())
                 .itemGroup(i.getItemGroup())
+                .areaGroup(i.getAreaGroup())
                 .price(i.getPrice())
                 .theme(i.getTheme())
                 .isPurchasable(i.isPurchasable())
--- src/main/java/doritos/doriroom/item/dto/response/UserItemResponse.java
+++ src/main/java/doritos/doriroom/item/dto/response/UserItemResponse.java
@@ -1,15 +1,19 @@
 package doritos.doriroom.item.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import doritos.doriroom.item.domain.*;
+import doritos.doriroom.tourApi.domain.AreaGroup;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserItemResponse (
         Long itemId,
         String name,
         String imageUrl,
         ItemType itemType,
-        ItemGroup itemGroup,
+        ItemGroup itemGroup, // COMMON or AREA
+        AreaGroup areaGroup, // nullable
         CollectionTheme theme, // nullable
         boolean isEquipped
 ){
@@ -21,6 +25,7 @@ public record UserItemResponse (
                 .imageUrl(i.getImageUrl())
                 .itemType(i.getItemType())
                 .itemGroup(i.getItemGroup())
+                .areaGroup(i.getAreaGroup())
                 .theme(i.getTheme())
                 .isEquipped(userItem.isEquipped())
                 .build();
