0c7ce444f109fbb5ceadbf3cd35bb80eba2092fd
docherryra <docherryra@gmail.com>
2025-08-05 02:06:57 +0900

    feat(repository): areaGroup 기반 조회 기능 추가

--- src/main/java/doritos/doriroom/item/repository/ItemRepository.java
+++ src/main/java/doritos/doriroom/item/repository/ItemRepository.java
@@ -2,12 +2,14 @@ package doritos.doriroom.item.repository;
 
 import doritos.doriroom.item.domain.Item;
 import doritos.doriroom.item.domain.ItemGroup;
+import doritos.doriroom.tourApi.domain.AreaGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Optional<Item> findById(Long itemId);
-    List<Item> findByItemGroup(ItemGroup itemGroup); //지역별 또는 일반과제 조회
+    Optional<Item> findById(Long itemId); // 아이템 단일 조회
+    List<Item> findByItemGroup(ItemGroup itemGroup); // COMMON 아이템 조회 or AREA 아이템 전체 조회
+    List<Item> findByItemGroupAndAreaGroup(ItemGroup itemGroup, AreaGroup areaGroup); // 특정 지역별 아이템 조회 (ex JEJU)
 }
--- src/main/java/doritos/doriroom/item/repository/UserItemRepository.java
+++ src/main/java/doritos/doriroom/item/repository/UserItemRepository.java
@@ -4,6 +4,7 @@ import doritos.doriroom.item.domain.Item;
 import doritos.doriroom.item.domain.ItemGroup;
 import doritos.doriroom.item.domain.ItemType;
 import doritos.doriroom.item.domain.UserItem;
+import doritos.doriroom.tourApi.domain.AreaGroup;
 import doritos.doriroom.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,10 +13,11 @@ import java.util.Optional;
 
 public interface UserItemRepository extends JpaRepository<UserItem, Long> {
     List<UserItem> findByUser(User user); // 유저가 보유한 전체 아이템 조회
-    List<UserItem> findByUserAndItem_ItemGroup(User user, ItemGroup itemGroup); // 유저가 보유한 전체 그룹별 아이템 조회
+    List<UserItem> findByUserAndItem_ItemGroup(User user, ItemGroup itemGroup); // 유저가 보유한 COMMON or AREA 아이템 조회
+    List<UserItem> findByUserAndItem_ItemGroupAndItem_AreaGroup(User user, ItemGroup itemGroup, AreaGroup areaGroup); // 유저의 특정 지역별 아이템 조회
+
     Optional<UserItem> findByUserAndItem(User user, Item item); // 유저가 보유한 아이템 단일 조회
     Optional<UserItem> findByUserAndItem_ItemTypeAndIsEquippedTrue(User user, ItemType itemType); // 유저가 착용한 타입별 아이템 조회
-
 
     boolean existsByUserAndItem(User user, Item item); // 이미 보유하고 있는지 확인
 }
