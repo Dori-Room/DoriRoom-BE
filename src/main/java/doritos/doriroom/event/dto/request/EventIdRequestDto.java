0e8997d60520f13942994d329e58430eea3f661d
judyzero <qozmcll@kookmin.ac.kr>
2025-08-05 11:01:04 +0900

    fix: eventDetail -> eventId로 파일명 변경

--- src/main/java/doritos/doriroom/event/dto/request/EventIdRequestDto.java
+++ src/main/java/doritos/doriroom/event/dto/request/EventIdRequestDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Schema(description = "축제 상세 조회 요청")
-public record EventDetailRequestDto(
+public record EventIdRequestDto(
     @Schema(description = "축제 ID", example = "002cecce-05e2-4dbf-8994-6ed0922a8722")
     @NotNull(message = "축제 ID는 필수입니다")
     UUID eventId
