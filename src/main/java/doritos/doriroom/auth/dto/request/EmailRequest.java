df56180f5c15b9f3de300d499e7895e9afa71254
docherryra <docherryra@gmail.com>
2025-08-09 23:58:08 +0900

    feat(mail): 메일 인증 요청 및 메일 인증 코드 확인 Dto 구현

--- src/main/java/doritos/doriroom/auth/dto/request/EmailRequest.java
+++ src/main/java/doritos/doriroom/auth/dto/request/EmailRequest.java
@@ -1,4 +1,11 @@
 package doritos.doriroom.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
 public class EmailRequest {
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    @NotBlank private String email;
 }
--- src/main/java/doritos/doriroom/auth/dto/request/EmailVerificationRequest.java
+++ src/main/java/doritos/doriroom/auth/dto/request/EmailVerificationRequest.java
@@ -1,4 +1,13 @@
 package doritos.doriroom.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class EmailVerificationRequest {
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    @NotBlank private String email;
+
+    @Pattern(regexp = "^[0-9]{6}$", message = "인증번호는 6자리 숫자여야 합니다.")
+    @NotBlank private String verificationCode;
 }
