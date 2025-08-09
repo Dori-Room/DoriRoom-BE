c20c09ea180fae5be178d721e37c4284ef6ca565
docherryra <docherryra@gmail.com>
2025-08-10 00:46:17 +0900

    feat(auth): login dto에 스웨거 예시 값 추가

--- src/main/java/doritos/doriroom/auth/dto/request/LoginRequestDto.java
+++ src/main/java/doritos/doriroom/auth/dto/request/LoginRequestDto.java
@@ -1,11 +1,14 @@
 package doritos.doriroom.auth.dto.request;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 public class LoginRequestDto {
     @NotBlank
+    @Schema(description = "로그인 아이디", example = "user1234")
     private String username;
     @NotBlank
+    @Schema(description = "비밀번호", example = "Passw0rd!")
     private String password;
 }
