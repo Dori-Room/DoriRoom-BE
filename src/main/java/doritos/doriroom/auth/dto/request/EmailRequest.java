cbc92ae28587d81eb4a51373923e69d98924b0d6
docherryra <docherryra@gmail.com>
2025-08-13 04:01:37 +0900

    feat(mail): email 요청 스웨거 예시 추가

--- src/main/java/doritos/doriroom/auth/dto/request/EmailRequest.java
+++ src/main/java/doritos/doriroom/auth/dto/request/EmailRequest.java
@@ -1,5 +1,6 @@
 package doritos.doriroom.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -7,5 +8,6 @@ import lombok.Getter;
 @Getter
 public class EmailRequest {
     @Email(message = "올바른 이메일 형식이어야 합니다.")
+    @Schema(description = "이메일", example = "user@example.com")
     @NotBlank private String email;
 }
