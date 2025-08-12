5e684d3692bb40d99d9cf76f1bb678c96e5ea443
docherryra <docherryra@gmail.com>
2025-08-13 04:01:37 +0900

    feat(mail): email 요청 스웨거 예시 추가

--- src/main/java/doritos/doriroom/auth/dto/request/EmailVerificationRequest.java
+++ src/main/java/doritos/doriroom/auth/dto/request/EmailVerificationRequest.java
@@ -1,5 +1,6 @@
 package doritos.doriroom.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,8 +9,10 @@ import lombok.Getter;
 @Getter
 public class EmailVerificationRequest {
     @Email(message = "올바른 이메일 형식이어야 합니다.")
+    @Schema(description = "이메일", example = "user@example.com")
     @NotBlank private String email;
 
     @Pattern(regexp = "^[0-9]{6}$", message = "인증번호는 6자리 숫자여야 합니다.")
+    @Schema(description = "인증 코드", example = "123456")
     @NotBlank private String verificationCode;
 }
