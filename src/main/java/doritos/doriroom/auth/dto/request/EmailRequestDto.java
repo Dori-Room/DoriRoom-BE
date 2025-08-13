07ca3906d1fefcdd4016ef3a29b0366672f79517
docherryra <docherryra@gmail.com>
2025-08-13 22:12:56 +0900

    chore(mail): dto 클래스명 통일

--- src/main/java/doritos/doriroom/auth/dto/request/EmailRequestDto.java
+++ src/main/java/doritos/doriroom/auth/dto/request/EmailRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class EmailRequest {
+public class EmailRequestDto {
     @Email(message = "올바른 이메일 형식이어야 합니다.")
     @Schema(description = "이메일", example = "user@example.com")
     @NotBlank private String email;
