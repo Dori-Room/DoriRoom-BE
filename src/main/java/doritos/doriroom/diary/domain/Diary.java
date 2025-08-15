592a5cec4ed736e3cd2c9528b3c850842d5a420f
judyzero <qozmcll@kookmin.ac.kr>
2025-08-15 13:14:48 +0900

    feat: 일기 엔티티 생성

---
+++ src/main/java/doritos/doriroom/diary/domain/Diary.java
@@ -0,0 +1,73 @@
+package doritos.doriroom.diary.domain;
+
+import doritos.doriroom.diary.dto.request.DiaryCreateRequestDto;
+import doritos.doriroom.user.domain.RoomVisibility;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "diary")
+@Getter @NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Diary {
+    @Id
+    private UUID diaryId;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ElementCollection
+    @Column(name = "image_url")
+    private List<String> imageUrls;
+
+    @Column(nullable = false)
+    private RoomVisibility diaryVisibility;
+
+    @Column(nullable = false)
+    private int likes;
+
+    @Column(nullable = false)
+    private LocalDate visitedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "event_id", nullable = false)
+    private UUID eventId;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @PrePersist
+    protected void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
+
+    public static Diary from(UUID userId, DiaryCreateRequestDto request){
+        return Diary.builder()
+            .diaryId(UUID.randomUUID())
+            .content(request.content())
+            .imageUrls(request.imageUrls())
+            .diaryVisibility(request.visibility())
+            .likes(0)
+            .visitedAt(request.visitedAt())
+            .createdAt(LocalDateTime.now())
+            .eventId(request.eventId())
+            .userId(userId)
+            .build();
+    }
+}
