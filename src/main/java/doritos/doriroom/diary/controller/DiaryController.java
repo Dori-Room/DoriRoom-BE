04cb499afbffa38980dd665db96ad9f9510b4d52
judyzero <qozmcll@kookmin.ac.kr>
2025-08-15 13:16:33 +0900

    feat: 일기 생성 API 구현

---
+++ src/main/java/doritos/doriroom/diary/controller/DiaryController.java
@@ -0,0 +1,33 @@
+package doritos.doriroom.diary.controller;
+
+import doritos.doriroom.diary.dto.request.DiaryCreateRequestDto;
+import doritos.doriroom.diary.dto.response.DiaryResponseDto;
+import doritos.doriroom.diary.service.DiaryService;
+import doritos.doriroom.global.dto.ApiResponse;
+import doritos.doriroom.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name="일기 관련", description = "일기 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/diary")
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    @Operation(summary = "일기 작성", description = "일기를 작성합니다.")
+    @PostMapping("/create")
+    public ApiResponse<DiaryResponseDto> createDiary(
+        @AuthenticationPrincipal User user,
+        @RequestBody @Valid DiaryCreateRequestDto request){
+        DiaryResponseDto response = diaryService.createDiary(user.getUserId(), request);
+        return ApiResponse.ok(response);
+    }
+}
