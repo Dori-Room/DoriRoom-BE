fbbfc5f3f0f8786e0f0b0e34bade40fdf4926dda
docherryra <docherryra@gmail.com>
2025-08-19 21:18:48 +0900

    feat(user): 마이페이지 관련 service 구현

--- src/main/java/doritos/doriroom/user/service/UserService.java
+++ src/main/java/doritos/doriroom/user/service/UserService.java
@@ -1,14 +1,29 @@
 package doritos.doriroom.user.service;
 
+import doritos.doriroom.auth.exception.InvalidPasswordException;
+import doritos.doriroom.s3.S3Uploader;
+import doritos.doriroom.user.domain.User;
+import doritos.doriroom.user.dto.request.ChangePasswordRequestDto;
+import doritos.doriroom.user.dto.request.UpdateProfileRequestDto;
+import doritos.doriroom.user.dto.response.ProfileImageResponseDto;
+import doritos.doriroom.user.dto.response.UserMyPageInfoDetailResponseDto;
+import doritos.doriroom.user.dto.response.UserMyPageInfoResponseDto;
 import doritos.doriroom.user.exception.DuplicateException;
+import doritos.doriroom.user.exception.UsernameNotFoundException;
 import doritos.doriroom.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+    private final S3Uploader s3Uploader;
 
     public void checkUsernameDuplicate(String username){
         if (userRepository.existsByUsername(username)) {
@@ -21,4 +36,79 @@ public class UserService {
         }
     }
 
+    // 내 정보 조회 (마이페이지)
+    @Transactional(readOnly = true)
+    public UserMyPageInfoResponseDto getUserInfo(User user){
+        User foundUser = userRepository.findByUserId(user.getUserId())
+                .orElseThrow(UsernameNotFoundException::new);
+
+        return UserMyPageInfoResponseDto.from(foundUser);
+    }
+
+    // 내 정보 조회 (수정 페이지)
+    @Transactional(readOnly = true)
+    public UserMyPageInfoDetailResponseDto getUserInfoDetail(User user){
+        User foundUser = userRepository.findByUserId(user.getUserId())
+                .orElseThrow(UsernameNotFoundException::new);
+
+        return UserMyPageInfoDetailResponseDto.from(foundUser);
+    }
+
+    // 닉네임 변경
+    @Transactional
+    public void updateProfile(User user, UpdateProfileRequestDto request){
+        User foundUser = userRepository.findByUserId(user.getUserId())
+                .orElseThrow(UsernameNotFoundException::new);
+
+        if (!foundUser.getNickname().equals(request.nickname())){ // 닉네임 변경 여부 확인
+            checkNicknameDuplicate(request.nickname()); // 닉네임 중복 검사
+        }
+
+        foundUser.setNickname(request.nickname()); // 닉네임 업데이트
+    }
+
+    // 프로필 이미지 변경
+    @Transactional
+    public ProfileImageResponseDto uploadProfileImage(User user, MultipartFile file){
+        User foundUser = userRepository.findByUserId(user.getUserId())
+                .orElseThrow(UsernameNotFoundException::new);
+
+        if (StringUtils.hasText(foundUser.getProfileImageUrl())) { // 기존 프로필 이미지가 있으면, s3에서 삭제
+            s3Uploader.deleteFile(foundUser.getProfileImageUrl());
+        }
+
+        String profileImageUrl = s3Uploader.uploadFile(file, "profile_image"); // s3에 업로드 후 url 반환
+
+        foundUser.setProfileImageUrl(profileImageUrl); // 새 이미지 url 반영
+
+        return new ProfileImageResponseDto(profileImageUrl);
+    }
+
+    // 프로필 이미지 삭제
+    @Transactional
+    public void deleteProfileImage(User user){
+        User foundUser = userRepository.findByUserId(user.getUserId())
+                .orElseThrow(UsernameNotFoundException::new);
+
+        if (StringUtils.hasText(foundUser.getProfileImageUrl())) { // 기존 프로필 이미지가 있는지 확인 없으면 수행하지 않음
+            s3Uploader.deleteFile(foundUser.getProfileImageUrl()); // s3에서 삭제
+            foundUser.setProfileImageUrl(null); // DB에서 삭제
+        }
+    }
+
+    // 비밀번호 변경
+    @Transactional
+    public void changePassword(User user, ChangePasswordRequestDto request) {
+        User foundUser = userRepository.findByUserId(user.getUserId())
+                .orElseThrow(UsernameNotFoundException::new);
+
+        if (!encoder.matches(request.currentPassword(), foundUser.getPassword())){ // 기존 비밀번호와 현재 입력한 비밀번호가 일치한지 확인
+            throw new InvalidPasswordException("현재 비밀번호가 일치하지 않습니다.");
+        }
+        if (!request.newPassword().equals(request.confirmPassword())){
+            throw new InvalidPasswordException("새 비밀번호가 일치하지 않습니다.");
+        }
+
+        foundUser.setPassword(encoder.encode(request.newPassword())); // 새 비밀번호를 암호화하여 저장
+    }
 }
