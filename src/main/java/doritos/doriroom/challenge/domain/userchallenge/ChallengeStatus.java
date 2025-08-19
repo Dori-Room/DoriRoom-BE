5dc5b8d3e755abc3e3e08e6a669730273ed6988d
docherryra <docherryra@gmail.com>
2025-08-20 02:48:40 +0900

    feat(challenge): 유저가 수행 중인 도전과제 도메인 설계, ChallengeStatus 지정

---
+++ src/main/java/doritos/doriroom/challenge/domain/userchallenge/ChallengeStatus.java
@@ -0,0 +1,9 @@
+package doritos.doriroom.challenge.domain.userchallenge;
+
+public enum ChallengeStatus {
+//    NOT_STARTED,
+    IN_PROGRESS, // 도전 중인 과제 (수동으로 도전 버튼 누르거나, 자동 집계되는 과제(횟수 누적)를 도전 중인 상태)
+    WAIT_REWARD, // 조건 달성 후 보상 수령 대기 상태
+    COMPLETED  // 보상 수령 후 완전히 달성 처리된 상태
+
+}
