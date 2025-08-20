87ce1ed1186ae95b026e72c7bf32e214d54bfc07
judyzero <qozmcll@kookmin.ac.kr>
2025-08-20 22:34:22 +0900

    feat: 인기있는 축제 쿼리문 작성

--- src/main/java/doritos/doriroom/event/repository/EventRepositoryCustom.java
+++ src/main/java/doritos/doriroom/event/repository/EventRepositoryCustom.java
@@ -2,9 +2,12 @@ package doritos.doriroom.event.repository;
 
 import doritos.doriroom.event.domain.Event;
 import doritos.doriroom.event.dto.request.EventItemFilterRequestDto;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface EventRepositoryCustom {
     Page<Event> findFiltered(EventItemFilterRequestDto filter, Pageable pageable);
+
+    List<Event> findPopularEvents(int limit);
 }
