0558efd666b8f4fceb9e2a2fc9acfec732a2371e
docherryra <docherryra@gmail.com>
2025-08-22 18:24:26 +0900

    feat(quiz): 퀴즈 및 문제 도메인 추가

---
+++ src/main/java/doritos/doriroom/quiz/domain/Question.java
@@ -0,0 +1,37 @@
+package doritos.doriroom.quiz.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+@Builder
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz; // 퀴즈와 다대일 매핑함
+
+    @Column(nullable = false)
+    private int sequence; // 퀴즈에서 몇 번 문제에 해당하는지 지정 (1번 문제, 2번 ..)
+
+    @Column(nullable = false, length = 500)
+    private String content; // 문제 내용
+
+    @Column(nullable = false) // 문제 답안 (OX퀴즈의 경우 option1을 O, 2를 X로 지정하여 사용)
+    private String option1;
+    @Column(nullable = false)
+    private String option2;
+
+    // 4지선다일 경우 사용, 2지선다면 null
+    private String option3;
+    private String option4;
+
+    @Column(nullable = false)
+    private int correctAnswer; // 정답 번호 (1, 2, 3, 4 중)
+}
---
+++ src/main/java/doritos/doriroom/quiz/domain/Quiz.java
@@ -0,0 +1,31 @@
+package doritos.doriroom.quiz.domain;
+
+import doritos.doriroom.challenge.domain.challenge.Challenge;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "quizzes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+@Builder
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id", nullable = false, unique = true)
+    private Challenge challenge; // 도전과제와 1대1 매핑
+
+    private String title; // 예: "경상도 지역 퀴즈"
+
+    // 해당 퀴즈에 포함될 문제 목록
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("sequence asc") // 문제 순서로 정렬
+    private List<Question> questions = new ArrayList<>();
+
+}
