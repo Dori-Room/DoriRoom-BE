f123dbe09170bee4e25e979b3afad882893dac74
judyzero <qozmcll@kookmin.ac.kr>
2025-08-24 16:10:17 +0900

    feat: 유저 조회수 저장 및 초기화 구현

--- src/main/java/doritos/doriroom/user/repository/UserRepository.java
+++ src/main/java/doritos/doriroom/user/repository/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -21,4 +22,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query("SELECT u FROM User u WHERE u.userId IN :userIds")
     List<User> findByUserIdIn(@Param("userIds") List<UUID> userIds);
+
+    // 모든 유저의 조회수를 0으로 초기화
+    @Modifying
+    @Query("UPDATE User u SET u.viewCount = 0")
+    int resetAllViewCounts();
 }
