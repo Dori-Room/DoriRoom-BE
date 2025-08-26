eb980593c7f2a0862824014ba13c720e3dc53168
docherryra <docherryra@gmail.com>
2025-08-26 19:36:57 +0900

    feat(follow): 필요 예외 추가

--- src/main/java/doritos/doriroom/follow/exception/CannotFollowSelfException.java
+++ src/main/java/doritos/doriroom/follow/exception/CannotFollowSelfException.java
@@ -1,4 +1,10 @@
 package doritos.doriroom.follow.exception;
 
-public class CannotFollowSelfException {
+import doritos.doriroom.global.exception.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class CannotFollowSelfException extends ApiException {
+    public CannotFollowSelfException() {
+        super(HttpStatus.BAD_REQUEST, "자신을 팔로우할 수 없습니다.");
+    }
 }
