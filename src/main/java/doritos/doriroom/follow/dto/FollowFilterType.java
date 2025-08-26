6325fea6ae3e579b7eb067ee11e379ef9743afa1
docherryra <docherryra@gmail.com>
2025-08-26 19:27:59 +0900

    feat(follow): 필요 dto 추가

--- src/main/java/doritos/doriroom/follow/dto/FollowFilterType.java
+++ src/main/java/doritos/doriroom/follow/dto/FollowFilterType.java
@@ -1,4 +1,7 @@
 package doritos.doriroom.follow.dto;
 
 public enum FollowFilterType {
+    RECENT,         // 최근 팔로우순
+    OLDEST,         // 오래된 팔로우순
+    BEST_FRIEND    // 단짝 친구만
 }
--- src/main/java/doritos/doriroom/follow/dto/response/FollowStatusResponseDto.java
+++ src/main/java/doritos/doriroom/follow/dto/response/FollowStatusResponseDto.java
@@ -1,4 +1,11 @@
 package doritos.doriroom.follow.dto.response;
 
-public class FollowStatusResponseDto {
-}
+import lombok.Builder;
+
+@Builder
+public record FollowStatusResponseDto (
+        boolean isFollowing,
+        boolean isFollowedBy,     // 상대가 나를 팔로우하는지
+        boolean isBestFriend,
+        boolean isMutualFollow    // 서로 팔로우인지
+){}
