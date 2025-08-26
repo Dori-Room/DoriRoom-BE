f3ad53e31bd56795d46d1b7c40007be422907dc0
docherryra <docherryra@gmail.com>
2025-08-26 19:36:57 +0900

    feat(follow): 필요 예외 추가

--- src/main/java/doritos/doriroom/follow/exception/FollowAlreadyExistsException.java
+++ src/main/java/doritos/doriroom/follow/exception/FollowAlreadyExistsException.java
@@ -1,4 +1,11 @@
 package doritos.doriroom.follow.exception;
 
-public class FollowAlreadyExistsException {
+import doritos.doriroom.global.exception.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class FollowAlreadyExistsException extends ApiException {
+    public FollowAlreadyExistsException() {
+        super(HttpStatus.CONFLICT, "이미 팔로우 중인 사용자입니다.");
+
+    }
 }
--- src/main/java/doritos/doriroom/follow/exception/FollowNotFoundException.java
+++ src/main/java/doritos/doriroom/follow/exception/FollowNotFoundException.java
@@ -1,4 +1,11 @@
 package doritos.doriroom.follow.exception;
 
-public class FollowNotFoundException {
+import doritos.doriroom.global.exception.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class FollowNotFoundException extends ApiException {
+    public FollowNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "팔로우 관계가 존재하지 않습니다.");
+
+    }
 }
