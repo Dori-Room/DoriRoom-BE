c8f8fa8bd4dd9920fb90d608a7e9951033843b3c
docherryra <docherryra@gmail.com>
2025-08-27 17:53:41 +0900

    feat(atlasReward): 보상 아이템 응답 정보 추가

--- src/main/java/doritos/doriroom/atlas/dto/response/AtlasRewardItemDto.java
+++ src/main/java/doritos/doriroom/atlas/dto/response/AtlasRewardItemDto.java
@@ -1,6 +1,9 @@
 package doritos.doriroom.atlas.dto.response;
 
 import doritos.doriroom.atlas.domain.AtlasReward;
+import doritos.doriroom.item.domain.CollectionTheme;
+import doritos.doriroom.item.domain.ItemType;
+import doritos.doriroom.tourApi.domain.AreaGroup;
 import lombok.Builder;
 
 
@@ -10,8 +13,12 @@ public record AtlasRewardItemDto(
         // 지역 도감 특정 기준 달성 시 받을 수 있는 보상 아이템 정보
 
         int targetLevel, // 해당 보상을 받을 수 있는 목표 레벨
+        Long itemId,
         String itemName,
-        String itemImageUrl
+        String itemImageUrl,
+        ItemType itemType,
+        AreaGroup areaGroup,
+        CollectionTheme theme
 
 ) {
     public static AtlasRewardItemDto from(AtlasReward reward) { // AtlasReward 엔티티를 받아 DTO로 변환
@@ -19,8 +26,12 @@ public record AtlasRewardItemDto(
 
         return AtlasRewardItemDto.builder()
                 .targetLevel(reward.getTargetLevel())
+                .itemId(reward.getRewardItem().getItemId())
                 .itemName(reward.getRewardItem().getName())
                 .itemImageUrl(reward.getRewardItem().getImageUrl())
+                .itemType(reward.getRewardItem().getItemType())
+                .areaGroup(reward.getRewardItem().getAreaGroup())
+                .theme(reward.getRewardItem().getTheme())
                 .build();
     }
 }
