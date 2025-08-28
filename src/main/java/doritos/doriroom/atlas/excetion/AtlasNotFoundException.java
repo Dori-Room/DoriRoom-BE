713141c0e2f2ef180bbe4477ae6d6bb96049757a
docherryra <docherryra@gmail.com>
2025-08-28 18:04:42 +0900

    feat(atlas): 필요 예외 추가

--- src/main/java/doritos/doriroom/atlas/excetion/AtlasNotFoundException.java
+++ src/main/java/doritos/doriroom/atlas/excetion/AtlasNotFoundException.java
@@ -7,4 +7,7 @@ public class AtlasNotFoundException extends ApiException {
     public AtlasNotFoundException() {
         super(HttpStatus.NOT_FOUND, "해당 지역의 Atlas를 찾을 수 없습니다.");
     }
+    public AtlasNotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, message);
+    }
 }
