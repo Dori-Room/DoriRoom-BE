95a0c5da003e57597477a9c452d48306d2fcac56
docherryra <docherryra@gmail.com>
2025-08-29 01:02:05 +0900

    feat(quiz): 퀴즈의 questions에 대한 유효성 검증 추가

--- src/main/java/doritos/doriroom/quiz/domain/Quiz.java
+++ src/main/java/doritos/doriroom/quiz/domain/Quiz.java
@@ -2,6 +2,7 @@ package doritos.doriroom.quiz.domain;
 
 import doritos.doriroom.challenge.domain.challenge.Challenge;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ public class Quiz {
     private String title; // 예: "경상도 지역 퀴즈"
 
     // 해당 퀴즈에 포함될 문제 목록
+    @NotEmpty
     @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("sequence asc") // 문제 순서로 정렬
     private List<Question> questions = new ArrayList<>();
