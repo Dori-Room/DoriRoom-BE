efaff6db721dd2dc0f4ac9a589c0a6966ce6514f
docherryra <docherryra@gmail.com>
2025-08-28 18:05:42 +0900

    refactor(atlas): 필요 없는 메서드 제거, 및 락 레포지토리 메서드 추가

--- src/main/java/doritos/doriroom/atlas/repository/AtlasRewardRepository.java
+++ src/main/java/doritos/doriroom/atlas/repository/AtlasRewardRepository.java
@@ -3,6 +3,8 @@ package doritos.doriroom.atlas.repository;
 import doritos.doriroom.atlas.domain.Atlas;
 import doritos.doriroom.atlas.domain.AtlasReward;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,12 +12,8 @@ import java.util.Optional;
 public interface AtlasRewardRepository extends JpaRepository<AtlasReward, Long> {
     List<AtlasReward> findByAtlasAndTargetLevel(Atlas atlas, int level); // 해당 지역도감의 보상아이템들 조회 시 (현재는 1개만)
 
-    // 유저의 현재 레벨 기준으로 받을 수 있는 바로 다음의 보상 아이템 조회
-    Optional<AtlasReward> findFirstByAtlasAndTargetLevelGreaterThanOrderByTargetLevelAsc(Atlas atlas, int currentLevel);
+    // 모든 atlas에 대한 모든 보상 아이템을 한 번에 조회
+    @Query("SELECT ar FROM AtlasReward ar WHERE ar.atlas.id IN :atlasIds ORDER BY ar.atlas.id, ar.targetLevel")
+    List<AtlasReward> findByAtlasIdInOrderByTargetLevel(@Param("atlasIds") List<Long> atlasIds);
 
-    // 특정 지역의 모든 보상 아이템을 레벨 순으로 조회
-    List<AtlasReward> findByAtlasOrderByTargetLevel(Atlas atlas);
-
-    // 특정 지역의 특정 레벨 이하 보상 아이템들 조회
-    List<AtlasReward> findByAtlasAndTargetLevelLessThanEqualOrderByTargetLevel(Atlas atlas, int level);
 }
--- src/main/java/doritos/doriroom/atlas/repository/UserAtlasRepository.java
+++ src/main/java/doritos/doriroom/atlas/repository/UserAtlasRepository.java
@@ -3,7 +3,9 @@ package doritos.doriroom.atlas.repository;
 import doritos.doriroom.atlas.domain.Atlas;
 import doritos.doriroom.atlas.domain.UserAtlas;
 import doritos.doriroom.user.domain.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,9 @@ import java.util.UUID;
 
 public interface UserAtlasRepository extends JpaRepository<UserAtlas, UUID> {
     Optional<UserAtlas> findByUserAndAtlas(User user, Atlas atlas); // 유저의 특정 지역 도감 조회
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<UserAtlas> findByUserAndAtlasWithLock(User user, Atlas atlas); // 락으로 동시 발생 제어(경험치 추가 시)
 
     List<UserAtlas> findByUser(User user); // 유저의 모든 지역도감 조회 시
 }
