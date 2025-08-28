d91898451aec73937f971b925afc1479fa46476b
docherryra <docherryra@gmail.com>
2025-08-28 18:43:06 +0900

    fix(atlas): 레포지토리 메서드에 @Query 어노테이션 추가

--- src/main/java/doritos/doriroom/atlas/repository/UserAtlasRepository.java
+++ src/main/java/doritos/doriroom/atlas/repository/UserAtlasRepository.java
@@ -6,6 +6,7 @@ import doritos.doriroom.user.domain.User;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ public interface UserAtlasRepository extends JpaRepository<UserAtlas, UUID> {
     Optional<UserAtlas> findByUserAndAtlas(User user, Atlas atlas); // 유저의 특정 지역 도감 조회
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT ua FROM UserAtlas ua WHERE ua.user = :user AND ua.atlas = :atlas")
     Optional<UserAtlas> findByUserAndAtlasWithLock(User user, Atlas atlas); // 락으로 동시 발생 제어(경험치 추가 시)
 
     List<UserAtlas> findByUser(User user); // 유저의 모든 지역도감 조회 시
