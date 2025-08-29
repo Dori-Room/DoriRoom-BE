c8acbc987167809feeed43d5931c816aadc77ef1
judyzero <qozmcll@kookmin.ac.kr>
2025-08-29 23:30:06 +0900

    fix: 지역코드와 시군구코드를 하나의 Dto로 묶어 요청

--- src/main/java/doritos/doriroom/event/dto/request/EventItemFilterRequestDto.java
+++ src/main/java/doritos/doriroom/event/dto/request/EventItemFilterRequestDto.java
@@ -6,11 +6,8 @@ import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public record EventItemFilterRequestDto(
-    @Schema(description = "지역코드 (1: 서울, 2: 인천)", example = "1")
-    Integer areaCode,
-
-    @Schema(description = "시군구코드 리스트", example = "[1, 2, 3]")
-    List<Integer> sigunguCodes,
+    @Schema(description = "지역 세트 리스트")
+    List<LocationFilterDto> locations,
 
     @Schema(description = "분야코드 리스트 (EV010100: 문화관광축제, EV010200: 문화예술축제, EV02: 공연, EV03: 행사)", example = "[\"EV010100\", \"EV010200\"]")
     List<String> categoryCodes,
