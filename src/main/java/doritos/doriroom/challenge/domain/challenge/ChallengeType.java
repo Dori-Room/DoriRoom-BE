f5b86da959ff25da328569ba66fe4703c415b282
docherryra <docherryra@gmail.com>
2025-08-31 15:13:57 +0900

    feat(challege): 이웃 집 방문, 방 조회수 N번 달성 과제 상태를 반영

--- src/main/java/doritos/doriroom/challenge/domain/challenge/ChallengeType.java
+++ src/main/java/doritos/doriroom/challenge/domain/challenge/ChallengeType.java
@@ -4,6 +4,7 @@ public enum ChallengeType {
     // 일반 과제 예시  (자동으로 횟수 누적 집계하여 달성 가능)
     WRITE_DIARY,        // 일기 작성
     VISIT_NEIGHBOR,     // 이웃 집 방문
+    REACH_VISIT_COUNT,      // 방 조회수 N번 달성
     REACH_NEIGHBOR_COUNT, // 이웃 N명 달성
     REACH_ROOM_COUNT,    // 방 좋아요 수 N개 달성
     COLLECT_ITEM,       // 아이템 수집
