464f06adc1a7a9262f6841d49d34c48e8bc44486
docherryra <docherryra@gmail.com>
2025-09-06 23:00:32 +0800

    feat(item): isDefault 필드 추가

--- src/main/java/doritos/doriroom/item/domain/Item.java
+++ src/main/java/doritos/doriroom/item/domain/Item.java
@@ -17,9 +17,6 @@ public class Item {
     @Column(nullable = false)
     private String name;
 
-//    @Column(nullable = false)
-//    private String imageUrl;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ItemType itemType;
@@ -40,4 +37,8 @@ public class Item {
 
     @Column(nullable = false)
     private boolean isPurchasable; // true: 구매 가능, false: 이벤트/한정 지급
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isDefault = false; // 아이템이 기본 아이템인지 확인
 }
