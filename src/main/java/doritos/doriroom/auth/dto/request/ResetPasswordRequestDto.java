3bc5b290645e4d55b7d28a194e50d0de34060c72
docherryra <docherryra@gmail.com>
2025-09-06 22:07:17 +0800

    refactor(user): 유효성 검증 추가 및 정규표현식 수정

--- src/main/java/doritos/doriroom/auth/dto/request/ResetPasswordRequestDto.java
+++ src/main/java/doritos/doriroom/auth/dto/request/ResetPasswordRequestDto.java
@@ -11,9 +11,12 @@ public record ResetPasswordRequestDto(
         @Schema(description = "이메일 주소", example = "user@example.com")
         @NotBlank String email,
 
+
+        @Pattern(regexp = "^[0-9]{6}$", message = "인증번호는 6자리 숫자여야 합니다.")
         @NotBlank String code, // 사용자가 받은 인증코드
 
-        @Pattern(regexp = "^(?=.{6,20}$)((?=.*[a-zA-Z])(?=.*\\d)|(?=.*[a-zA-Z])(?=.*[!@#$%^&*])|(?=.*\\d)(?=.*[!@#$%^&*])).*$",
+
+        @Pattern(regexp = "^(?=.{8,20}$)((?=.*[a-zA-Z])(?=.*\\d)|(?=.*[a-zA-Z])(?=.*[!@#$%^&*])|(?=.*\\d)(?=.*[!@#$%^&*])).*$",
                 message = "비밀번호는 영문, 숫자, 특수문자 중 2가지 이상을 조합하여 6~20자로 설정해야 합니다.")
         @Schema(description = "비밀번호(영문/숫자/특수문자 포함 8~20자)", example = "Passw0rd!")
         @NotBlank String newPassword
