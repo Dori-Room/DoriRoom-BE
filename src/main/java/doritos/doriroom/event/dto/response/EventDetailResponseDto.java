29aa505ac1bd3d4586ded29ec245055e39e04b3f
docherryra <docherryra@gmail.com>
2025-09-08 23:11:26 +0900

    feat(event): 축제 조회 시 관련 도전과제의 id를 응답에 추가

--- src/main/java/doritos/doriroom/event/dto/response/EventDetailResponseDto.java
+++ src/main/java/doritos/doriroom/event/dto/response/EventDetailResponseDto.java
@@ -6,54 +6,57 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public record EventDetailResponseDto(
-    String title,
-    int contentId,
-    UUID eventId,
-    String startDate,
-    String endDate,
-    String addr1,
-    String addr2,
-    String areaName,
-    int areaCode,
-    String firstImage,
-    String secondImage,
-    int favoriteCount,
-    long diaryCount,
+        String title,
+        int contentId,
+        UUID eventId,
+        String startDate,
+        String endDate,
+        String addr1,
+        String addr2,
+        String areaName,
+        int areaCode,
+        String firstImage,
+        String secondImage,
+        int favoriteCount,
+        long diaryCount,
 
-    //상세정보
-    String sponsor1,
-    String sponsor2,
-    String useTimeFestival,
+        //상세정보
+        String sponsor1,
+        String sponsor2,
+        String useTimeFestival,
 
-    String eventIntro,
-    String eventContent,
+        String eventIntro,
+        String eventContent,
 
-    //위치정보
-    Double mapX,
-    Double mapY
+        //위치정보
+        Double mapX,
+        Double mapY,
+
+        Long relatedChallengeId // 관련 도전과제Id (없으면 null)
 ) {
-    public static EventDetailResponseDto from(Event event){
+    public static EventDetailResponseDto from(Event event, Long relatedChallengeId) {
         return new EventDetailResponseDto(
-            event.getTitle(),
-            event.getContentId(),
-            event.getEventId(),
-            event.getStartDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
-            event.getEndDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
-            event.getAddr1(),
-            event.getAddr2(),
-            AreaGroup.getAreaNameByAreaCode(event.getAreaCode()),
-            event.getAreaCode(),
-            event.getFirstImage(),
-            event.getSecondImage(),
-            event.getFavoriteCount(),
-            event.getDiaryCount(),
-            event.getSponsor1(),
-            event.getSponsor2(),
-            event.getUseTimeFestival(),
-            event.getEventIntro(),
-            event.getEventContent(),
-            event.getMapX(),
-            event.getMapY()
+                event.getTitle(),
+                event.getContentId(),
+                event.getEventId(),
+                event.getStartDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
+                event.getEndDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
+                event.getAddr1(),
+                event.getAddr2(),
+                AreaGroup.getAreaNameByAreaCode(event.getAreaCode()),
+                event.getAreaCode(),
+                event.getFirstImage(),
+                event.getSecondImage(),
+                event.getFavoriteCount(),
+                event.getDiaryCount(),
+                event.getSponsor1(),
+                event.getSponsor2(),
+                event.getUseTimeFestival(),
+                event.getEventIntro(),
+                event.getEventContent(),
+                event.getMapX(),
+                event.getMapY(),
+                relatedChallengeId
         );
     }
 }
