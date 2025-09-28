832b3b79d6f3af78edf8b9c682e5ba1cbdb46e2f
docherryra <docherryra@gmail.com>
2025-09-28 22:28:28 +0900

    refactor(notification): 도전과제 최초 달성 시 알람 발송

--- src/main/java/doritos/doriroom/challenge/service/ChallengeService.java
+++ src/main/java/doritos/doriroom/challenge/service/ChallengeService.java
@@ -188,10 +188,15 @@ public class ChallengeService {
             int currentProgress = userChallenge.getCurrentProgress() + count;
             userChallenge.setCurrentProgress(Math.max(0, currentProgress)); // 0 미만으로 내려가지 않도록 방지
 
+            ChallengeStatus previousStatus = userChallenge.getStatus();
+
             // 과제 진척도에 따른 과제 상태 변환
             if (currentProgress >= challenge.getTargetCount()){
                 userChallenge.setStatus(ChallengeStatus.WAIT_REWARD); // 현재 진척도가 TargetCount보다 크면 보상 대기
-                notificationService.sendNotification(user, NotificationType.CHALLENGE_REWARD, challenge.getTitle());
+
+                if (previousStatus != ChallengeStatus.WAIT_REWARD) { // 최초 보상 수령 가능 시 알람 전송
+                    notificationService.sendNotification(user, NotificationType.CHALLENGE_REWARD, challenge.getTitle());
+                }
             } else if (currentProgress > 0){ // TargetCount보다 작으면 IN_PROGRESS로 변경 (보상 받지 않았는데 이후 진척도가 줄면 재달성 요구함)
                 userChallenge.setStatus(ChallengeStatus.IN_PROGRESS);
             } else { // 진척도가 0이면 미시작 상태로 지정함
@@ -227,11 +232,16 @@ public class ChallengeService {
                 continue;
             }
 
+            ChallengeStatus previousStatus = userChallenge.getStatus();
+
             // totalCount를 받아 진행도에 반영
             userChallenge.setCurrentProgress(totalCount);
             if (userChallenge.getCurrentProgress() >= challenge.getTargetCount()){
                 userChallenge.setStatus(ChallengeStatus.WAIT_REWARD);
-                notificationService.sendNotification(user, NotificationType.CHALLENGE_REWARD, challenge.getTitle());
+
+                if (previousStatus != ChallengeStatus.WAIT_REWARD) { // 최초 보상 수령 가능 시 알람 전송
+                    notificationService.sendNotification(user, NotificationType.CHALLENGE_REWARD, challenge.getTitle());
+                }
             } else if (totalCount == 0){
                 userChallenge.setStatus(ChallengeStatus.NOT_STARTED);
             } else if (totalCount > 0){
