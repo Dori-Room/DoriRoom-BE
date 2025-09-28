e41d5a5be1fa3ad65a103db514c96d59cf0ef8d6
docherryra <docherryra@gmail.com>
2025-09-28 22:06:40 +0900

    feat(fcm): config 클래스 추가

--- src/main/java/doritos/doriroom/global/config/FirebaseConfig.java
+++ src/main/java/doritos/doriroom/global/config/FirebaseConfig.java
@@ -4,24 +4,54 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FirebaseConfig {
+    @Value("${firebase.project_id}")
+    private String projectId;
+
+    @Value("${firebase.client_email}")
+    private String clientEmail;
+
+    @Value("${firebase.client_id}")
+    private String clientId;
+
+    @Value("${firebase.private_key_id}")
+    private String privateKeyId;
+
+    @Value("${firebase.private_key}")
+    private String privateKey;
+
     @PostConstruct
     public void init() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/firebase/doriroom-firebase-admin.json");
+        String formattedKey = privateKey.replace("\n", "\\n");
 
+        String json = String.format("""
+        {
+          "type": "service_account",
+          "project_id": "%s",
+          "private_key_id": "%s",
+          "private_key": "%s",
+          "client_email": "%s",
+          "client_id": "%s"
+        }
+        """, projectId, privateKeyId, formattedKey, clientEmail, clientId);
+
+        try (ByteArrayInputStream stream = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8))) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(stream);
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(credentials)
                     .build();
 
-            FirebaseApp.initializeApp(options);
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
         }
     }
 }
