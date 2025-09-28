ec7066eb6970c59a8dffdd63711eb59e33ce7f06
judyzero <qozmcll@kookmin.ac.kr>
2025-09-28 10:35:38 +0900

    feat: 여러 사용자의 장착 아이템 조회 메소드 추가

--- src/main/java/doritos/doriroom/item/repository/UserItemRepository.java
+++ src/main/java/doritos/doriroom/item/repository/UserItemRepository.java
@@ -6,10 +6,14 @@ import doritos.doriroom.item.domain.ItemType;
 import doritos.doriroom.item.domain.UserItem;
 import doritos.doriroom.tourApi.domain.AreaGroup;
 import doritos.doriroom.user.domain.User;
+import java.util.Set;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserItemRepository extends JpaRepository<UserItem, Long> {
     List<UserItem> findByUser(User user); // 유저가 보유한 전체 아이템 조회
@@ -22,4 +26,13 @@ public interface UserItemRepository extends JpaRepository<UserItem, Long> {
 
     boolean existsByUserAndItem(User user, Item item); // 이미 보유하고 있는지 확인
     List<UserItem> findByUserAndEquippedTrue(User user); // 착용 중인 아이템들 조회
+
+    @Query("""
+    SELECT ui FROM UserItem ui
+    JOIN FETCH ui.item i
+    JOIN FETCH ui.user u
+    WHERE u.userId IN :userIds
+    AND ui.equipped = true
+    """)
+    List<UserItem> findByUser_UserIdInAndIsEquippedTrue(@Param("userIds") Set<UUID> userIds);
 }
