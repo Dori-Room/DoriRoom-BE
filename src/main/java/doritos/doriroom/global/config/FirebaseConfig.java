20e8f240f8b7eae27a5b0b1aca928617f0bebc3b
docherryra <docherryra@gmail.com>
2025-09-28 19:35:38 +0900

    feat(fcm): config 클래스 추가

---
+++ src/main/java/doritos/doriroom/global/config/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package doritos.doriroom.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Component
+public class FirebaseConfig {
+    @PostConstruct
+    public void init() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/firebase/doriroom-firebase-admin.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
