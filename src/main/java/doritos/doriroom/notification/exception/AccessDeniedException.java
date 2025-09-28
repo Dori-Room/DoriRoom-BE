a2dcda8868ac9786cacc612239d4bb10f93c59ac
docherryra <docherryra@gmail.com>
2025-09-28 23:37:49 +0900

    chore(notification): http status 변경

--- src/main/java/doritos/doriroom/notification/exception/AccessDeniedException.java
+++ src/main/java/doritos/doriroom/notification/exception/AccessDeniedException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class AccessDeniedException extends ApiException {
     public AccessDeniedException() {
-        super(HttpStatus.BAD_REQUEST, "해당 알림을 읽을 권한이 없습니다.");
+        super(HttpStatus.FORBIDDEN, "해당 알림을 읽을 권한이 없습니다.");
     }
 }
