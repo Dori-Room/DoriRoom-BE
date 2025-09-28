b7bfa0eb8d94d9b85791e5b73aa963c2bbdfc856
docherryra <docherryra@gmail.com>
2025-09-28 17:22:54 +0900

    feat(fcm): notification 알림 타입 정의 enum 추가

---
+++ src/main/java/doritos/doriroom/notification/domain/NotificationType.java
@@ -0,0 +1,8 @@
+package doritos.doriroom.notification.domain;
+
+public enum NotificationType {
+    FOLLOWER,        // 새로운 팔로워
+    GUESTBOOK_ENTRY, // 새로운 방명록
+    CHALLENGE_REWARD,    // 도전과제 보상 수령
+    DIARY_LIKE           // 내 일기 좋아요
+}
