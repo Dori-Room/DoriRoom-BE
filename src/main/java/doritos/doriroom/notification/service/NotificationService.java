7fb5ddd1084aa390e451305fc9bcee9e88574d63
docherryra <docherryra@gmail.com>
2025-09-28 23:32:28 +0900

    chore: 미사용 import문 제거

--- src/main/java/doritos/doriroom/notification/service/NotificationService.java
+++ src/main/java/doritos/doriroom/notification/service/NotificationService.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
