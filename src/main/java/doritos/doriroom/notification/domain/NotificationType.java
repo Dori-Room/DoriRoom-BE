10a7e3b86145c51d570bc0ed7d8f7d40bded334b
IyeonHan <104899885+docherryra@users.noreply.github.com>
2025-10-02 20:33:16 +0900

    feat(notifi): 알림 발송 테스트 API 추가 (#166)
    * feat(notifi): 알림 발송 테스트 API 추가
    
    * feat(notifi): http 메서드 매핑 어노테이션 누락 수정

--- src/main/java/doritos/doriroom/notification/domain/NotificationType.java
+++ src/main/java/doritos/doriroom/notification/domain/NotificationType.java
@@ -8,7 +8,9 @@ public enum NotificationType {
     FOLLOWER("%s님이 회원님을 팔로우하기 시작했습니다."),
     GUESTBOOK_ENTRY("%s님이 방명록을 남겼습니다."),
     CHALLENGE_REWARD("도전과제 '%s'를 완료했습니다! 보상을 받아주세요."),
-    DIARY_LIKE("%s님이 회원님의 일기를 좋아합니다.");
+    DIARY_LIKE("%s님이 회원님의 일기를 좋아합니다."),
+
+    TEST_MESSAGE("테스트 메세지 발송!");
 
     private final String message;
 
