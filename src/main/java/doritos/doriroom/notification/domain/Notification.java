ea9a4732ea31c7097b41f5dc620f95f4c41f6475
docherryra <docherryra@gmail.com>
2025-10-04 15:41:27 +0900

    chore(notifi): 안쓰는 setter 제거

--- src/main/java/doritos/doriroom/notification/domain/Notification.java
+++ src/main/java/doritos/doriroom/notification/domain/Notification.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "notifications")
-@Getter @Setter
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 @Builder
 public class Notification {
