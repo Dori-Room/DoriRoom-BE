968d10cbb32d5f9b168e10b1b13325aa28a5f9fe
docherryra <docherryra@gmail.com>
2025-10-08 00:47:15 +0900

    feat(auth): 회원탈퇴를 하드 딜리트로 구현, 관련 유저 데이터 전부 삭제:

--- src/main/java/doritos/doriroom/guestbook/repository/GuestbookRepository.java
+++ src/main/java/doritos/doriroom/guestbook/repository/GuestbookRepository.java
@@ -1,9 +1,11 @@
 package doritos.doriroom.guestbook.repository;
 
 import doritos.doriroom.guestbook.domain.Guestbook;
+import doritos.doriroom.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,8 @@ public interface GuestbookRepository extends JpaRepository<Guestbook, UUID> {
     
     @Query("SELECT g FROM Guestbook g WHERE g.roomOwnerId = :roomOwnerId ORDER BY g.createdAt DESC")
     Page<Guestbook> findByRoomOwnerIdOrderByCreatedAtDesc(@Param("roomOwnerId") UUID roomOwnerId, Pageable pageable);
+
+    @Modifying
+    @Query("DELETE FROM Guestbook g WHERE g.writerId = :userId OR g.roomOwnerId = :userId")
+    void deleteAllByUserId(@Param("userId") UUID userId);
 }
