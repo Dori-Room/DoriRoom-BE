dae78893b5dae0eea751bea38ab9354b9f1b3b4f
docherryra <docherryra@gmail.com>
2025-10-07 23:10:10 +0900

    feat(notification): 알림 목록 응답에 관련 id 추가

--- src/main/java/doritos/doriroom/notification/dto/NotificationResponseDto.java
+++ src/main/java/doritos/doriroom/notification/dto/NotificationResponseDto.java
@@ -2,8 +2,10 @@ package doritos.doriroom.notification.dto;
 
 import doritos.doriroom.notification.domain.Notification;
 import doritos.doriroom.notification.domain.NotificationType;
+import doritos.doriroom.user.domain.User;
 import lombok.Builder;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Builder
 public record NotificationResponseDto(
@@ -11,6 +13,7 @@ public record NotificationResponseDto(
         String content,
         NotificationType type,
         boolean isRead,
+        String targetId,
         LocalDateTime createdAt
 ) {
     public static NotificationResponseDto from(Notification notification) {
@@ -19,6 +22,7 @@ public record NotificationResponseDto(
                 .content(notification.getContent())
                 .type(notification.getType())
                 .isRead(notification.isRead())
+                .targetId(notification.getTargetId())
                 .createdAt(notification.getCreatedAt())
                 .build();
     }
